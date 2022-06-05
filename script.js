19cff1826bb100db7c4788086ee2867649de6335
Abdallah Mohammed <sofrex8@gmail.com>
2022-06-05 17:31:41 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,241 @@
+var myList = [
+        { "CarName": "Mercedes AMG ONE",
+            "Make": "Mercedes",
+            "Model": "AMG ONE",
+            "RentalPrice": 5000,
+            "KilometersTraveled": "160000",
+            "Latitude": 21.576968,
+            "Longitude": 39.140480,
+            "Images": [
+                "https://www.drivearabia.com/app/uploads/2022/06/mercedes-amg-one-9-1.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/06/mercedes-amg-one-8-1.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/06/mercedes-amg-one-2-1.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/06/mercedes-amg-one-7-1.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/06/mercedes-amg-one-6-1.jpg"
+            ],
+            "Available From": "2022-06-04T15:15:10+00:00",
+            "Available Until ": "2022-06-09T15:15:10+00:00"
+        },
+        { "CarName": "Ferrari 296 GTS",
+            "Make": "Ferrari",
+            "Model": "296 GTS",
+            "RentalPrice": 7000,
+            "KilometersTraveled": "10000",
+            "Latitude": 21.577706,
+            "Longitude": 39.127460,
+            "Images": [
+                "https://www.drivearabia.com/app/uploads/2022/05/ferrari-296-gts.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/05/ferrari-296-gts-1.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/05/ferrari-296-gts-2.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/05/ferrari-296-gts-4.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/05/ferrari-296-gts-4.jpg"
+            ],
+            "Available From": "2022-05-01T15:15:10+00:00",
+            "Available Until ": "2022-06-01T15:15:10+00:00"
+        },
+        {
+            "CarName": "Cadillac Escalade-V",
+            "Make": "Cadillac",
+            "Model": "Escalade-V",
+            "RentalPrice": 899,
+            "KilometersTraveled": "13000",
+            "Latitude": 21.580883,
+            "Longitude": 39.110540,
+            "Images": [
+                "https://www.drivearabia.com/app/uploads/2022/05/2023-cadillac-escalade-v-front-view.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/05/2023-cadillac-escalade-v-front-view-1.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/05/2023-cadillac-escalade-v-interior.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/05/2023-cadillac-escalade-v-rear-view-1.jpg"
+            ],
+            "Available From": "2022-05-01T15:15:10+00:00",
+            "Available Until ": "2022-06-01T15:15:10+00:00"
+        },
+        {
+            "CarName": "Toyota GR Corolla",
+            "Make": "Toyota",
+            "Model": "GR Corolla",
+            "RentalPrice": 700,
+            "KilometersTraveled": "20000",
+            "Latitude": 21.595408,
+            "Longitude": 39.115181,
+            "Images": ["https://www.drivearabia.com/app/uploads/2022/04/2023-toyota-gr-corolla-circuit-edition-sliding.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/04/2023-toyota-gr-corolla-circuit-edition-interior.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/04/2023-toyota-gr-corolla-circuit-edition-sliding-1.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/04/2023-toyota-gr-corolla-circuit-edition-sliding-3.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/04/2023-toyota-gr-corolla-circuit-edition-sliding-4.jpg"],
+            "Available From": "2022-05-01T15:15:10+00:00",
+            "Available Until ": "2022-06-01T15:15:10+00:00"
+        },
+        {
+            "CarName": "Mercedes-AMG EQE 53",
+            "Make": "Mercedes",
+            "Model": "AMG EQE 53",
+            "RentalPrice": 1899,
+            "KilometersTraveled":"70000",
+            "Latitude":21.602256,
+            "Longitude":39.135451,
+            "Images":["https://www.drivearabia.com/app/uploads/2022/02/mercedes-amg-eqe-53-6.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/mercedes-amg-eqe-53-5.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/mercedes-amg-eqe-53-3.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/mercedes-amg-eqe-53-7.jpg"
+            ],
+            "Available From":"2022-05-01T15:15:10+00:00",
+            "Available Until ":"2022-06-01T15:15:10+00:00"
+        },
+        {
+            "CarName": "Citroën C5 Aircross",
+            "Make": "Citroën",
+            "Model": "C5 Aircross",
+            "RentalPrice": 450,
+            "KilometersTraveled":"100000",
+            "Latitude":21.612598,
+            "Longitude":39.125244,
+            "Images":["https://www.drivearabia.com/app/uploads/2022/02/citroen-c5-aircross-2022.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/citroen-c5-aircross-2022-3.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/citroen-c5-aircross-2022-5.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/citroen-c5-aircross-2022-9.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/citroen-c5-aircross-2022-7.jpg"
+            ],
+            "Available From":"2022-05-01T15:15:10+00:00",
+            "Available Until ":"2022-06-01T15:15:10+00:00"
+        },
+        {
+            "CarName": "Mazda CX-50",
+            "Make": "Mazda",
+            "Model": "CX-50",
+            "RentalPrice": 500,
+            "KilometersTraveled":"100000",
+            "Latitude":21.621479,
+            "Longitude":39.151623,
+            "Images":["https://www.drivearabia.com/app/uploads/2022/02/mazda-cx-50-1.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/mazda-cx-50-7.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/mazda-cx-50-2.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/mazda-cx-50-3.jpg"],
+            "Available From":"2022-05-01T15:15:10+00:00",
+            "Available Until ":"2022-06-01T15:15:10+00:00"
+        },
+        {
+            "CarName": "Kia Niro",
+            "Make": "Kia",
+            "Model": "Niro",
+            "RentalPrice": 500,
+            "KilometersTraveled":"50000",
+            "Latitude":21.534895,
+            "Longitude":39.173363,
+            "Images":["https://www.drivearabia.com/app/uploads/2022/02/2023-kia-niro-3.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/2023-kia-niro-4.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/2023-kia-niro-5.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/2023-kia-niro.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/2023-kia-niro-6.jpg"],
+            "Available From":"2022-05-01T15:15:10+00:00",
+            "Available Until ":"2022-06-01T15:15:10+00:00"
+        },
+        {
+            "CarName": "Chevrolet Blazer",
+            "Make": "Chevrolet",
+            "Model": "Blazer",
+            "RentalPrice": 5000,
+            "KilometersTraveled":"80000",
+            "Latitude":21.492845,
+            "Longitude":39.230353,
+            "Images":["https://www.drivearabia.com/app/uploads/2022/02/2023-chevrolet-blazerrs-002.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/2023-chevrolet-blazerrs-004.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/2023-chevrolet-blazer-dash.jpg",
+                "https://www.drivearabia.com/app/uploads/2022/02/2023-chevrolet-blazer-side-view.jpg"
+            ],
+            "Available From":"2022-05-01T15:15:10+00:00",
+            "Available Until ":"2022-06-01T15:15:10+00:00"
+        }
+
+    ] ;
+
+
+var widget = document.querySelector(".js-widget");
+
+function distance(lat2, lon2) {
+    var lat1 = 21.570491;
+    var lon1 = 39.149440;
+
+    if ((lat1 === lat2) && (lon1 === lon2)) {
+        return 0;
+    }
+    else {
+        var radlat1 = Math.PI * lat1/180;
+        var radlat2 = Math.PI * lat2/180;
+        var theta = lon1-lon2;
+        var radtheta = Math.PI * theta/180;
+        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+            dist = 1;
+        }
+        dist = Math.acos(dist);
+        dist = dist * 180/Math.PI;
+        dist = dist * 60 * 1.1515;
+        dist = Math.ceil(dist * 1.609344);
+        return dist;
+    }
+}
+
+function buildList(carNum) {
+    if (document.querySelector(".button")) {
+        document.querySelector(".button").outerHTML = "";
+    }
+
+    var widgetContent = "";
+
+
+    for (let i = 0; i < carNum; i++) {
+
+
+        widgetContent +=
+            '<a class="car-link" href="CarPage.html">' +
+            '<div class="car-card">' +
+            "<h2>" +
+            myList[i].CarName +
+            "</h2>" +
+            '<img src="' +
+            myList[i].Images[0] +
+            '">' +
+            "<ul>" +
+            "<li>" +
+           '<i class="fas fa-tools">\xa0</i>'+
+            myList[i].Make +
+            "</li>" +
+            "<li>" +
+            '<i class="fas fa-car">\xa0</i>'+
+            myList[i].Model +
+            "</li>" +
+            "<li>" +
+            '<i class="far fa-money-bill-alt">\xa0</i>'+
+            myList[i].RentalPrice +
+            "SAR" +
+            "</li>" +
+            "</ul>" +
+            "<ul>" +
+            "<li>" +
+            '<i class="fa fa-road">\xa0</i>'+
+            myList[i].KilometersTraveled +
+            " KM" +
+            "</li>" +
+            "<li>" +
+            '<i class="fa fa-location-arrow">\xa0</i>'+
+            distance(myList[i].Latitude, myList[i].Longitude)+
+            " KM " +
+            "</li>" +
+            "<li>" +
+            '<i class="fas fa-calendar-alt">\xa0</i>'+
+            "Avaliable From "+
+            new Date(myList[i]["Available From"]).getDate() + "/"+ new Date(myList[i]["Available From"]).getMonth() + "/" +new Date(myList[i]["Available From"]).getFullYear()+
+            "</li>" +
+
+            "</ul>" +
+            "</div>" +
+            "</a>";
+    }
+
+    widget.insertAdjacentHTML("beforeend", widgetContent);
+}
+
+
+
+buildList(9);
